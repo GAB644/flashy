803a27dfa9abd71c7d81af117586fdbbda21b456
Gabriel Carmo <gabriel.carmo.vip@gmail.com>
2026-05-20 18:34:25 +0100

    fixed a couple of stuff

--- holidays.js
+++ holidays.js
@@ -1,10 +1,14 @@
 // not sure if this fucking works or not. but changing the date did nothing prob doesnt work at all sadders
 (function() {
-    function setBanner(imgSrc, bgSrc) {
+    function setBanner(imgSrc, bannerBgSrc, bodyBgColor) {
         var bannerImg = document.querySelector('.banner img');
         if (bannerImg) bannerImg.src = imgSrc;
         var banner = document.querySelector('.banner');
-        if (banner) banner.style.backgroundImage = "url('" + bgSrc + "')";
+        if (banner) banner.style.backgroundImage = "url('" + bannerBgSrc + "')";
+        var body = document.querySelector('body');
+        if (body && bodyBgColor) {
+            body.style.backgroundColor = bodyBgColor;
+        }
     }
 
     document.addEventListener('DOMContentLoaded', function() {
@@ -12,12 +16,12 @@
         var month = now.getMonth(); // 0-11
 
         if (month === 11) {
-            setBanner('images/bannercristmas.png', 'images/bannercribg.png');
+            setBanner('images/bannercristmas.png', 'images/bannercribg.png', '#f8fafc');
             return;
         }
 
         if (month === 9) {
-            setBanner('images/bannerhalloween.png', 'images/bannerhallbg.png');
+            setBanner('images/bannerhalloween.png', 'images/bannerhallbg.png', '#1a1209');
             return;
         }
     });
